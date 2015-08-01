0dc15490d9165be7f56e73faf6bdf15b377d7cdb
root <root@localhost.localdomain>
2015-08-01 17:03:38 +0000

    Crontab Jobs script

---
+++ apps/server/script/cron_jobs.sh
@@ -0,0 +1,7 @@
+echo `date '+%Y-%m-%d %H:%M:%S'` >> "/home/www/TheOne/apps/crawler.log"
+
+cd /home/www/TheOne/apps/crawler
+python product_hunt.py
+
+cd /home/www/TheOne/apps/crawler
+python hacker_news.py
