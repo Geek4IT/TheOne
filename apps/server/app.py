34d90a66734207b3ecc55086c42de44ef627db02
GEEK4IT <chunjianye@gmail.com>
2015-08-02 03:47:00 -0400

    Change the skeleton, tornado best pratice

---
+++ apps/server/app.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import tornado.httpserver
+import tornado.ioloop
+from tornado.options import options
+import tornado.web
+
+from settings import settings
+from urls import url_patterns
+
+class TheOneApp(tornado.web.Application):
+	def __init__(self):
+		tornado.web.Application.__init__(self, url_patterns, **settings)
+
+def main():
+	app = TheOneApp()
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+	main()
---
+++ apps/server/handlers/feed.py
@@ -0,0 +1,69 @@
+import gzip
+import json
+import StringIO
+import pymongo
+from utils.db_utils import get_mongodb_conn
+from settings import conn 
+from bson import ObjectId
+from tornado.web import RequestHandler, asynchronous
+
+
+class IndexHandler(RequestHandler):
+	def get(self):
+		self.render("base.html")
+
+class JSONEncoder(json.JSONEncoder):
+	def default(self,obj):
+		if isinstance(obj, ObjectId):
+			return str(obj)
+		return json.JSONEncoder.default(self,obj)
+
+def zip_content(content):
+    buf = StringIO.StringIO()
+    content = JSONEncoder().encode(content)
+    # content = json.dumps(content)
+    fp = gzip.GzipFile(mode='wb', compresslevel=9, fileobj=buf)
+    fp.write(str(content))
+    fp.close()
+    return buf.getvalue()
+
+def finish_request(request, content, compress=False):
+    request.set_header("Content-Type", 'application/Json; charset="utf-8"')
+    if compress:
+        request.set_header('Content-Encoding', 'gzip')
+        request.write(zip_content(content))
+    else:
+        request.write(content)
+    request.finish()
+
+class FeedsHandler(RequestHandler):
+	@asynchronous
+	def get(self):
+		source = self.get_argument("source", None)
+
+		conn = get_mongodb_conn()
+		db = conn.news
+		table = db.feed
+		feedsVal = None
+		if source:
+			feedsVal = table.find({"source":source})
+		else:
+			feedsVal = table.find()
+
+		print feedsVal.count()
+		feeds = list(feedsVal)
+
+		finish_request(self,feeds,True)
+
+class FeedHandler(RequestHandler):
+	@asynchronous
+	def get(self):
+		
+		conn = get_mongodb_conn()
+		db = conn.news
+		table = db.feed
+		feedsVal = table.find()
+		print feedsVal.count()
+		feeds = list(feedsVal)
+
+		finish_request(self,feeds,True)
